fix return bugs. fix logprobs bugs

src/storage.py
@@ -85,7 +85,7 @@ class Dataset(object):
         self.means, self.logvars = actor(self.states)
         self.means = self.means.detach()
         self.logvars = self.logvars.detach()
-        self.logprobs = (-0.5*self.logvars - (self.actions - self.means).pow(2) / 2*torch.exp(self.logvars))
+        self.logprobs = (-0.5*self.logvars - (self.actions - self.means).pow(2) / (2*torch.exp(self.logvars)))
         self.logprobs = torch.sum(self.logprobs, dim=1, keepdim=True)
 
     def data_generator(self, batch_size):
@@ -117,6 +117,7 @@ class Dataset(object):
         rets = torch.zeros_like(rewards) # [T,1]
         for t in reversed(range(len(rewards))):
             rets[t] = rewards[t] + return_so_far * gamma * (1-news[t])
+            return_so_far = rets[t]
         return rets
 
 