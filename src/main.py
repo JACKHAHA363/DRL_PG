90c1c7d752fc7d2c1043530a43aad5c75bd93840
Yuchen Lu <luyuchen.paul@gmail.com>
2018-05-14 23:02:55 -0400

    maximum episode length matters a log

--- src/main.py
+++ src/main.py
@@ -23,24 +23,26 @@ def parser_args():
                         help='interval between training status logs (default: 10)')
     parser.add_argument('--baseline', type=str, default=None,
                         help="[None]|[value]|[model]")
-    parser.add_argument('--T', default=100, type=int, help="maximum length of each episode")
+    parser.add_argument('--T', default=50, type=int, help="maximum length of each episode")
     parser.add_argument('--batch_size', default=10000, type=int,
                         help='number of transitions each iteration')
     parser.add_argument('--steps', default=10000, type=int, help="number of policy updates")
 
     # policy optimization
-    parser.add_argument('--actor_epochs', default=1, type=int)
+    parser.add_argument('--actor_epochs', default=4, type=int)
     parser.add_argument('--actor_bsz', default=32, type=int)
+    parser.add_argument('--clip_range', default=0.2, type=float,
+                        help="range for ratio clipping in surrogate loss")
+    parser.add_argument('--max_grad_norm', default=0.5, type=float,
+                        help="clip grad norm")
 
     # critic optimization
     parser.add_argument('--critic_epochs', default=1, type=int)
-    parser.add_argument('--critic_bsz', default=32, type=int)
-
-    # ppo
-    parser.add_argument('--kl_coef', default=0.1, type=float)
+    parser.add_argument('--critic_bsz', default=128, type=int)
 
     # entropy
     parser.add_argument('--ent_coef', default=0.01, type=float)
+
 
     args = parser.parse_args()
     return args
@@ -62,8 +64,8 @@ def main():
                                 action_dim=env.action_space.shape[0],
                                 num_hidden=10)
     critic = MLPCritic(state_dim=env.observation_space.shape[0], num_hidden=10)
-    actor_opt = optim.Adam(actor.parameters(), lr=1e-4)
-    critic_opt = optim.Adam(critic.parameters(), lr=1e-4)
+    actor_opt = optim.Adam(actor.parameters(), lr=1e-4, eps=1e-5)
+    critic_opt = optim.Adam(critic.parameters(), lr=1e-4, eps=1e-5)
 
     memory = RollOut()
 
@@ -82,8 +84,10 @@ def main():
                 values = critic(batch['states'])
                 targets = batch['returns']
                 critic_loss = (targets - values).pow(2).mean()
+
                 critic_opt.zero_grad()
                 critic_loss.backward()
+                torch.nn.utils.clip_grad_norm_(critic.parameters(), args.max_grad_norm)
                 critic_opt.step()
 
         # train policy
@@ -91,6 +95,7 @@ def main():
             actor_data_gen = dataset.data_generator(args.actor_bsz)
             for batch in actor_data_gen:
                 rets = batch['returns']
+
                 if args.baseline == 'value':
                     baseline = batch['values']
                 elif args.baseline == 'model':
@@ -110,8 +115,11 @@ def main():
                 curr_logprobs = curr_dist.log_prob(actions)
 
                 # surrogate loss
-                surrogate_loss = - torch.exp(curr_logprobs - old_logprobs) * (rets - baseline)
-                surrogate_loss = torch.mean(surrogate_loss)
+                advs = rets - baseline
+                ratio = torch.exp(curr_logprobs - old_logprobs)
+                surr1 = ratio * advs
+                surr2 = torch.clamp(ratio, 1.0 - args.clip_range, 1.0 + args.clip_range) * advs
+                surrogate_loss = -torch.min(surr1, surr2).mean()
 
                 # add correction bias term
                 if args.baseline == 'world':
@@ -121,17 +129,25 @@ def main():
                 entropy = curr_dist.entropy().mean()
                 surrogate_loss -= args.ent_coef * entropy
 
+                # add KL term
+                KL = 0.5 * (old_logprobs - curr_logprobs).pow(2)
+                KL = KL.mean()
+
                 actor_opt.zero_grad()
                 surrogate_loss.backward()
+                torch.nn.utils.clip_grad_norm_(actor.parameters(), args.max_grad_norm)
                 actor_opt.step()
 
         # record statistics
         if (step+1) % args.log_interval == 0:
             stats = runner.stats
-            print('------- iter{} ----------'.format(step+1))
+            print('------- iter{} ----------'.format(step + 1))
             for key in stats.keys():
                 print("{} : {:.3f}".format(key, stats[key]))
-                writer.add_scalar(key, stats[key], step+1)
+                writer.add_scalar(key, stats[key], step + 1)
+            discounted_return = dataset.returns[0].item()
+            print("discounted return: {:.3f}".format(discounted_return))
+            writer.add_scalar('discounted return', discounted_return, step + 1)
 
 
 if __name__ == '__main__':
